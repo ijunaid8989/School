ebda13bc6dd8d5033680662be35490b12e6df9d4
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-08 08:18:18 +0500

    removed finally password hash and salt now its just password

---
+++ db/migrate/20150808031442_change_pasword_hash_to_password.rb
@@ -0,0 +1,5 @@
+class ChangePaswordHashToPassword < ActiveRecord::Migration
+  def change
+     rename_column :users, :password_hash, :password
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808020524) do
+ActiveRecord::Schema.define(version: 20150808031442) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password_hash"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
