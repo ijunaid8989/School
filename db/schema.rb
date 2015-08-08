17949fc64112bc8d1e06e8e37d6b058efe6db48c
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-08 07:07:46 +0500

    Removed one coulmn form Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807083551) do
+ActiveRecord::Schema.define(version: 20150808020524) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_hash"
-    t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
