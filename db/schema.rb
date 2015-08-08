011894d03d8fa14f70ef5cc631a1b7aa200677ea
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-08 10:43:26 +0500

    Changed password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808031442) do
+ActiveRecord::Schema.define(version: 20150808053942) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
