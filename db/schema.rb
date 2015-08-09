73f2815bb9e2bba801d55e90c4d7b53bbcac1ba0
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-09 13:25:00 +0500

    Added Menu as global partials type in layout main

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808053942) do
+ActiveRecord::Schema.define(version: 20150809075715) do
+
+  create_table "levels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
