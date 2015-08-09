12417c1dd2d32a9f8e42ede7cace9bcec8fc31fc
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-09 18:17:53 +0500

    Added level done

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,5 +1,20 @@
 class LevelsController < ApplicationController
   def new
   	@level = Level.new
+  	@alevel = Level.all()
   end
+
+  def create
+  	@level = Level.new(level_params)
+  	if @level.save
+  		redirect_to '/addlevel'
+  	end
+  end
+
+private
+
+def level_params
+	params.require(:level).permit(:name)
+end
+
 end
